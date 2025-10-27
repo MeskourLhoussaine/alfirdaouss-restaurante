4c87bf0d63e030651361684313bf5cc81970d492
MeskourLhoussaine <meskour.lhoussaine642@gmail.com>
2025-10-27 18:25:19 +0100

    Menu coment

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -3,10 +3,10 @@ import './Menu.css';
 import Section from '../../pages/Menu/Section';
 
 
-import { Card, CardBody, CardText, CardTitle, Collapse } from 'react-bootstrap';
+/*import { Card, CardBody, CardText, CardTitle, Collapse } from 'react-bootstrap';*/
 
-import { motion } from 'framer-motion';
-import { FaCircle, FaChevronDown, FaChevronUp } from 'react-icons/fa';
+/*import { motion } from 'framer-motion';
+import { FaCircle, FaChevronDown, FaChevronUp } from 'react-icons/fa';*/
 import BreakfastImg from '../../utils/images/pizza-bg.jpg';
 import LunchImg from '../../utils/images/lunch-img.jpg';
 import DinnerImg from '../../utils/images/dinner-img.jpg';
